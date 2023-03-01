478fab22a0a9940005e88aa17c480699d5f24d96
Emil Leonard Aastrøm <emillaa@stud.ntnu.no>
2023-03-01 14:27:38 +0100

    Update to unit tests (@beforeeach & display names)

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,4 +1,7 @@
 import edu.ntnu.idatt2001.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -8,42 +11,47 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
-    Passage testPassage = new Passage("First passage in test", "Content of first passage");
-    Story testStory = new Story("Test story", testPassage);
-    List<Goal> goalList = new ArrayList<>();
+    Passage testPassage;
+    Story testStory;
+    List<Goal> goalList;
+    Player player;
+    Game myGame;
 
-    Player player = new Player("Kåre", 100, 50, 25);
-    Game myGame = new Game(player,testStory, goalList);
+
+    @BeforeEach
+    @DisplayName("Initialize variables with test data")
+    void init(){
+    testPassage = new Passage("First passage in test", "Content of first passage");
+    testStory = new Story("Test story", testPassage);
+    goalList = new ArrayList<>();
+    player = new Player("Kåre", 100, 50, 25);
+    myGame = new Game(player,testStory, goalList);
+    }
 
     @Test
+    @DisplayName("Ensure that method getPlayer() returns correct variable")
     void getPlayerReturnsPlayer() {
         assertEquals(player, myGame.getPlayer());
     }
 
     @Test
-    void getPlayerDoesNotReturnStory() {
-        assertNotEquals(player, myGame.getStory());
-    }
-
-
-    @Test
+    @DisplayName("Ensure that getStory() returns correct story")
     void getStoryReturnsCorrectStory() {
         assertEquals(testStory, myGame.getStory());
-        assertNotEquals(testStory, myGame.getGoals());
-        assertNotEquals(testStory, myGame.getClass());
-        assertNotEquals(testStory, myGame.getPlayer());
-
     }
 
     @Test
+    @DisplayName("Testing that getGoals method returns correct goals")
     void getGoals() {
     }
 
     @Test
+    @DisplayName("")
     void begin() {
     }
 
     @Test
+    @DisplayName("")
     void go() {
     }
 }
