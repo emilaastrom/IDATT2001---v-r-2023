53f207f5ac715497ba1150b366c00807f2ebc301
Emil Leonard Aastrøm <emillaa@stud.ntnu.no>
2023-03-28 15:46:16 +0200

    Updates FileHandlerTest to test the new feature readFile()

--- src/test/java/edu/ntnu/idatt2001/FileHandlerTest.java
+++ src/test/java/edu/ntnu/idatt2001/FileHandlerTest.java
@@ -14,7 +14,9 @@ public class FileHandlerTest {
     exampleStory.getOpeningPassage().addLink(new Link("Link1", "New link in opening passage"));
     exampleStory.getOpeningPassage().addLink(new Link("Link2", "Another link in opening passage"));
     Passage passage = new Passage("Title of test passage 2", "Content of test passage 2");
-    passage.addLink(new Link("Test", "Test"));
+    passage.addLink(new Link("Test link", "Test reference"));
+    passage.addLink(new Link("Test link 2", "Test reference 2"));
+    passage.addLink(new Link("Test link 3", "Test reference 3"));
     exampleStory.addPassage(passage);
     exampleStory.addPassage(new Passage("Title of test passage 3", "Content of test passage 3"));
     exampleStory.addPassage(new Passage("Title of test passage 4", "Content of test passage 4"));
@@ -26,6 +28,13 @@ public class FileHandlerTest {
   public void writeFile(){
     //Testing that the method writeFile() writes the correct story to the file
     FileHandler.writeFile(exampleStory);
+  }
+
+  @Test
+  @DisplayName("Testing readFile() method")
+  public void readFile(){
+    Story story = FileHandler.readFile("exampleStory.paths");
+    FileHandler.writeFile(story);
   }
 
 
