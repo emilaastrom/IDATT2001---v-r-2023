2789ffeb24ae0f037bc5a98d78ef0497cf9fc40c
Emil Leonard Aastrøm <emillaa@stud.ntnu.no>
2023-04-11 14:30:06 +0200

    Adding background art and icons.

--- src/main/java/edu/ntnu/idatt2001/PathsApplication.java
+++ src/main/java/edu/ntnu/idatt2001/PathsApplication.java
@@ -1,8 +1,11 @@
 package edu.ntnu.idatt2001;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
@@ -11,6 +14,7 @@ import javafx.scene.paint.Stop;
 import javafx.scene.shape.Box;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class PathsApplication extends Application {
     public static void main(String[] args) {
@@ -22,21 +26,17 @@ public class PathsApplication extends Application {
         //STACKPANE FOR DIFFERENT WINDOWS
         StackPane windowStackPane = new StackPane();
 
+
+
         //ENTRYWINDOW
         VBox entryWindowVBox = new VBox();
         entryWindowVBox.setPrefWidth(500);
         entryWindowVBox.setAlignment(javafx.geometry.Pos.CENTER);
         entryWindowVBox.setSpacing(30);
 
-        Text entryWindowText = new Text("(PATHS LOGO PLACEMENT)");
-        entryWindowText.setStyle(
-                "-fx-font-size: 24px; " +
-                "-fx-font-weight: bold; " +
-                "-fx-padding: 10px 20px 10px 20px; " +
-                "-fx-border-width: 1px; " +
-                "-fx-border-radius: 5px; " +
-                "-fx-background-radius: 5px");
-        entryWindowText.setFill(javafx.scene.paint.Color.WHITE);
+        Image entryWindowLogo = new Image("file:src/main/resources/templogo.png");
+        StackPane entryWindowLogoStackPane = new StackPane();
+        entryWindowLogoStackPane.getChildren().add(new javafx.scene.image.ImageView(entryWindowLogo));
 
         Button entryWindowChooseAdventureButton = new Button("CHOOSE YOUR ADVENTURE (.paths file)");
         entryWindowChooseAdventureButton.setStyle(
@@ -49,19 +49,21 @@ public class PathsApplication extends Application {
                 "-fx-padding: 10px 20px 10px 20px; " +
                 "-fx-text-fill: rgb(255,255,255); " +
                 "-fx-background-radius: 5px;");
-        entryWindowVBox.getChildren().addAll(entryWindowText, entryWindowChooseAdventureButton);
+        entryWindowVBox.getChildren().addAll(entryWindowLogoStackPane, entryWindowChooseAdventureButton);
 
         BorderPane entryWindow = new BorderPane();
-        entryWindow.setVisible(false);
+        entryWindow.setVisible(true);
         entryWindow.setCenter(entryWindowVBox);
-        entryWindow.setStyle("-fx-background-color: rgb(23,56,23)");
+        entryWindow.setStyle("-fx-background-color: rgb(0,0,0,0)");
+
+
 
         //PATHSWINDOW
         BorderPane pathsWindowCenterBox = new BorderPane();
         pathsWindowCenterBox.setPrefWidth(500);
-        pathsWindowCenterBox.setPrefHeight(200);
+        pathsWindowCenterBox.setPrefHeight(400);
         pathsWindowCenterBox.setStyle(
-                "-fx-background-color: rgb(0,0,0);" +
+                "-fx-background-color: rgb(0,0,0,0.5);" +
                 "-fx-border-color: rgb(255,255,255);" +
                 "-fx-border-width: 5px;"
         );
@@ -70,10 +72,37 @@ public class PathsApplication extends Application {
         pathsWindowBottomBox.setPrefWidth(500);
         pathsWindowBottomBox.setPrefHeight(100);
         pathsWindowBottomBox.setStyle(
-                "-fx-background-color: rgb(0,0,0);" +
+                "-fx-background-color: rgb(0,0,0,0.5);" +
                 "-fx-border-color: rgb(255,255,255);" +
                 "-fx-border-width: 5px;"
         );
+
+        HBox pathsWindowBottomBoxHBox = new HBox();
+        pathsWindowBottomBoxHBox.setSpacing(30);
+        pathsWindowBottomBoxHBox.setAlignment(Pos.CENTER_LEFT);
+        pathsWindowBottomBoxHBox.setPadding(new javafx.geometry.Insets(0, 0, 0, 50));
+        ImageView pathsWindowBottomBoxHBoxImageViewScore = new ImageView("file:src/main/resources/score.png");
+        ImageView pathsWindowBottomBoxHBoxImageViewHeart = new ImageView("file:src/main/resources/heart.png");
+        ImageView pathsWindowBottomBoxHBoxImageViewCoin = new ImageView("file:src/main/resources/coin.png");
+        ImageView pathsWindowBottomBoxHBoxImageViewChest = new ImageView("file:src/main/resources/chest.png");
+        pathsWindowBottomBoxHBox.setFillHeight(true);
+
+        pathsWindowBottomBoxHBox.getChildren().addAll(pathsWindowBottomBoxHBoxImageViewScore,
+                pathsWindowBottomBoxHBoxImageViewHeart,
+                pathsWindowBottomBoxHBoxImageViewCoin,
+                pathsWindowBottomBoxHBoxImageViewChest);
+        pathsWindowBottomBox.setLeft(pathsWindowBottomBoxHBox);
+
+        HBox pathsWindowBottomBoxHBox2 = new HBox();
+        pathsWindowBottomBoxHBox2.setSpacing(30);
+        pathsWindowBottomBoxHBox2.setAlignment(Pos.CENTER_RIGHT);
+        pathsWindowBottomBoxHBox2.setPadding(new javafx.geometry.Insets(0, 50, 0, 0));
+        ImageView pathsWindowBottomBoxHBox2ImageViewHelp = new ImageView("file:src/main/resources/help.png");
+        ImageView pathsWindowBottomBoxHBox2ImageViewSettings = new ImageView("file:src/main/resources/settings.png");
+        pathsWindowBottomBoxHBox2.getChildren().addAll(
+                pathsWindowBottomBoxHBox2ImageViewHelp,
+                pathsWindowBottomBoxHBox2ImageViewSettings);
+        pathsWindowBottomBox.setRight(pathsWindowBottomBoxHBox2);
 
         VBox pathsWindowVBox = new VBox();
         pathsWindowVBox.setSpacing(30);
@@ -83,13 +112,28 @@ public class PathsApplication extends Application {
         BorderPane pathsWindow = new BorderPane();
         pathsWindow.setCenter(pathsWindowVBox);
         pathsWindow.setPadding(new javafx.geometry.Insets(50, 200, 50, 200));
-        pathsWindow.setVisible(true);
-        pathsWindow.setStyle("-fx-background-color: rgb(23,56,23)");
+        pathsWindow.setVisible(false);
+        pathsWindow.setStyle("-fx-background-color: rgb(0,0,0,0)");
+
+
 
         //General JavaFX settings
+        BackgroundRepeat backgroundRepeat = BackgroundRepeat.REPEAT;
+        BackgroundPosition backgroundPosition = BackgroundPosition.CENTER;
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(
+                new javafx.scene.image.Image("file:src/main/resources/background.jpg"),
+                backgroundRepeat,
+                backgroundRepeat,
+                backgroundPosition,
+                backgroundSize);
+        Background background = new Background(backgroundImage);
+
         windowStackPane.getChildren().addAll(entryWindow, pathsWindow);
         BorderPane root = new BorderPane();
         root.setCenter(windowStackPane);
+        root.setBackground(background);
+
 
         Scene scene = new Scene(root, 1500  , 750);
         stage.setTitle("Paths");
